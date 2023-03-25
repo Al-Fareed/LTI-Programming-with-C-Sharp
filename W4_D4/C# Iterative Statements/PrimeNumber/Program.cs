a302fd6b93573d51aaa5a0ff589d047d923fc781
Al-Fareed <alfareedss472@gmail.com>
2023-03-25 12:54:56 +0530

    w3 3rd program

---
+++ W4_D4/C# Iterative Statements/PrimeNumber/Program.cs
@@ -0,0 +1,47 @@
+﻿/*
+Finding Prime Number
+
+Write a program to find whether a given number is prime or not.
+
+
+Input Format:
+
+Input consists of a single integer.
+
+Output Format:
+
+The output should display whether the input is “Prime” or “Not prime”.
+Refer sample input and output for formatting specifications.
+
+
+Sample Input 1:
+13
+Sample Output1:
+Prime
+
+
+Sample Input 2:
+33
+Sample Output2:
+Not prime
+*/
+using System;
+class Program{
+    static void Main(){
+        int n = int.Parse(Console.ReadLine());
+        bool prime;
+        for(int i = 3; i < n; i++){
+            if(n%i==0){
+                prime=false;
+                break;
+            }else{
+                prime=true;
+            }
+        }
+        if(prime){
+            Console.WriteLine("Prime");
+        }else{
+            Console.WriteLine("Not prime");
+        }
+    }
+}
