eebe4676ce36f97a045bc2155857949e58d146a6
Al-Fareed <alfareedss472@gmail.com>
2023-03-27 23:17:26 +0530

    completed 2 programs of week5 day

---
+++ W5_D1/C# Polymorphism/Overloading/Stall.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Stall
+{
+    public void ComputeCost(string type,int ft){
+        Console.WriteLine("Gold costs Rs.{0}",(100*ft));
+    }  
+    public void ComputeCost(string type, int ft, int noOfTv){
+        Console.WriteLine("Diamond costs Rs.{0}",((150*ft)+(noOfTv*100)));
+    }  
+    public void ComputeCost(string type, int ft, bool ch){
+        Console.WriteLine("Platinum costs Rs.{0}",((200*ft)+(ch==true?1000:0)));
+    }  
+      
+}
