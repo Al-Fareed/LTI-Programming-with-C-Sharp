f983c43ae945a3d373b6a25c41e59453382eb220
Al-Fareed <alfareedss472@gmail.com>
2023-04-09 07:01:53 +0530

    minor changes

--- Week 4 - 5/W4_D3/Understanding C# Operators/SeriresGeneration/Program.cs
+++ Week 4 - 5/W4_D3/Understanding C# Operators/SeriresGeneration/Program.cs
@@ -12,16 +12,16 @@ Sample Output:
 
 */
 //Below code works but not recommended
-using System;
-class Program {
-    static void Main() {
-        int n = int.Parse(Console.ReadLine());
-        int[] num={1,3, 4, 8, 15, 27, 50,92,169,311,572,1052 ,1935 ,3559 ,6546 ,12040, 22145, 40731, 74916, 137792, 253439, 466147, 857378, 1576964, 2900489, 5334831, 9812284, 18047604, 33194719, 61054607, 112296930, 206546256, 379897793, 698740979, 1285185028};
-        for(int i=0;i<n;i++){
-            Console.Write(num[i]+" ");
-        }
-    }
-}
+// using System;
+// class Program {
+//     static void Main() {
+//         int n = int.Parse(Console.ReadLine());
+//         int[] num={1,3, 4, 8, 15, 27, 50,92,169,311,572,1052 ,1935 ,3559 ,6546 ,12040, 22145, 40731, 74916, 137792, 253439, 466147, 857378, 1576964, 2900489, 5334831, 9812284, 18047604, 33194719, 61054607, 112296930, 206546256, 379897793, 698740979, 1285185028};
+//         for(int i=0;i<n;i++){
+//             Console.Write(num[i]+" ");
+//         }
+//     }
+// }
 
 // Actual code for generating
 class Program{
--- Week 4 - 5/W4_D4/C# Iterative Statements/PrimeNumber/Program.cs
+++ Week 4 - 5/W4_D4/C# Iterative Statements/PrimeNumber/Program.cs
@@ -29,16 +29,16 @@ using System;
 class Program{
     static void Main(){
         int n = int.Parse(Console.ReadLine());
-        bool prime;
+        bool isPrime = false;
         for(int i = 3; i < n; i++){
             if(n%i==0){
-                prime=false;
+                isPrime=false;
                 break;
             }else{
-                prime=true;
+                isPrime=true;
             }
         }
-        if(prime){
+        if(isPrime){
             Console.WriteLine("Prime");
         }else{
             Console.WriteLine("Not prime");
