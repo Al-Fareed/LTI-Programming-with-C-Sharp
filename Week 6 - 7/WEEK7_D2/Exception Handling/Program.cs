f12478ddf8a8b7aacfd96c72ef866228ec85b9dd
Al-Fareed <alfareedss472@gmail.com>
2023-04-14 22:25:57 +0530

    First Program

---
+++ Week 6 - 7/WEEK7_D2/Exception Handling/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args){
+        try
+        {
+            Console.WriteLine("Enter the item type name");
+            string iName = Console.ReadLine();
+
+            Console.WriteLine("Enter the deposit");
+            int deposit = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the cost per day");
+            int costPerDay = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Item type details\nName:"+iName);
+            Console.WriteLine("Deposit:"+deposit);
+            Console.WriteLine("Cost per day:"+costPerDay);
+        }
+        catch (System.Exception)
+        {
+            Console.WriteLine("Input string was not in the correct format");
+        }
+    }
+}
