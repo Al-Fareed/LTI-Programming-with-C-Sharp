9c77073982a84ba3b884b96b0a5b529f0e858bdb
Al-Fareed <alfareedss472@gmail.com>
2023-04-16 00:04:43 +0530

    adding quwstion as comment

--- Week 6 - 7/WEEK7_D2/HallNotAvailableException/Program.cs
+++ Week 6 - 7/WEEK7_D2/HallNotAvailableException/Program.cs
@@ -1,4 +1,117 @@
-﻿using System;
+﻿/*
+
+HallNotAvailableException
+[Note :
+Strictly adhere to the object oriented specifications given as a part of the problem statement.
+Follow the naming conventions as mentioned. Create separate classes in separate files.]
+
+Create class Event with the following private variables/attributes.
+Data Type	Variable
+string	_name
+string	_detail
+string	_type
+string	_organizer
+
+Include appropriate properties.
+Include default constructor and a four argument constructor with parameters in the following order: _name, _detail,  _type,  _organizer.
+
+Create class Hall with the following private attributes/variables.
+Data Type	Variable
+string 	_name
+long	_mobileNumber
+double	_costPerDay
+string	_owner
+
+Include appropriate properties.
+Include default constructor and a four argument constructor with parameters in the following order: _name,  _mobileNumber, _costPerDay,  _owner.
+
+Create class HallBooking with the following private variables/attributes.
+Data Type	Variable
+Hall	_hallInstance
+Event	_eventInstance
+DateTime	_startDate
+DateTime	_endDate
+double	_price
+
+Include appropriate properties.
+Include default constructor and a five argument constructor with parameters in the following order _eventInstance, _hallInstance, _startDate,  _endDate, _price.
+
+Create class HallBookingBO with the following method.
+Method	Description
+public bool ValidateHallBooking(List<HallBooking> bookings, HallBooking hallBooking)	In this method, check the availablity of the hall and return true if available, else throw HallNotAvailableException exception.
+
+Create class HallNotAvailableException which extends Exception class.
+Include a Constructor with an argument "Message" pass the message to the base class constructor.
+
+Create Program class with Main method.
+Pre-populate the following hall details and store it into a list
+Name	Mobile number	Cost per day	Owner
+Great Hall	9874563210	1000.00	Daniel
+Royal Hall	9865321470	1500.00	Cassandra
+Gibson Hall	8796543211	750.00	Abraham
+Queen Hall	8745693221	5000.00	William
+
+Pre-populate the following event details and store it into a list
+Name	Detail	Type	Organizer
+SARC Conference	International Conference	Conference	Peter
+Birthday party	Albert's Birthday party	Birthday	Daniel
+Christmas Eve	Christmas celebration	Stage show	John
+New Year Eve	New Year celebration	Stage show	Smith
+
+Create Program class with Main method, display the pre-populated event and hall details in the specified format
+String.Format("{0} {1,-15} {2,-28} {3,-20} {4,-5}", "S.No","Name", "Detail", "Type", "Organizer")
+String.Format("{0} {1,-10} {2,15} {3,15} {4,15}", "S.No", "Name", "Mobile number", "Cost per day", "Owner")
+
+Get the hall booking details from the user.
+Validate the availability of the hall and book it.
+Use try catch block to catch the exception and display the error message. 
+
+ 
+Input and Output Format
+Refer sample input and output for formatting specifications.
+All text in bold corresponds to the input and the rest corresponds to output.
+
+Sample Input and Output :
+Event Details
+S.No Name            Detail                       Type                 Organizer
+1 SARC Conference International Conference     Conference           Peter
+2 Birthday party  Albert's Birthday party      Birthday             Daniel
+3 Christmas Eve   Christmas celebration        Stage show           John
+4 New Year Eve    New Year celebration         Stage show           Smith
+Hall Details
+S.No Name         Mobile number    Cost per day           Owner
+1 Great Hall      9874563210            1000          Daniel
+2 Royal Hall      9865321470            1500       Cassandra
+3 Gibson Hall      8796543211             750         Abraham
+4 Queen Hall      8745693221            5000         William
+Enter the hall id
+1
+Enter the event id
+1
+Enter the start date
+12/12/2018
+Enter the end date
+01/01/2019
+Enter the price
+5400.00
+Booked Successfully
+Do you want to book a hall?(yes/no)
+yes
+Enter the hall id
+1
+Enter the event id
+2
+Enter the start date
+12/12/2018
+Enter the end date
+01/01/2019
+Enter the price
+8795.00
+Hall Not Available Exception
+Do you want to book a hall?(yes/no)
+no
+*/
+using System;
 using System.Collections.Generic;
 
 namespace HallBookingSystem
