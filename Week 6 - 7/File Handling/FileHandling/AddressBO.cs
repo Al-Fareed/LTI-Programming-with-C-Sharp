99eeff5d0756a67fb0628978a72e823214736032
Al-Fareed <alfareedss472@gmail.com>
2023-04-17 11:01:55 +0530

    changed property name while displaying

--- Week 6 - 7/File Handling/FileHandling/AddressBO.cs
+++ Week 6 - 7/File Handling/FileHandling/AddressBO.cs
@@ -20,7 +20,7 @@ public class AddressBO
         //fill code here
         foreach (Address item in list)
         {
-            Console.WriteLine("{0,-15} {1,-25} {2,-15} {3,-15} {4,-15}",item.AddressLine1,item.AddressLine2,item.City,item.State,item.PinCode);
+            Console.WriteLine("{0,-15} {1,-25} {2,-15} {3,-15} {4,-15}",item.AddressLine1,item.AddressLine2,item.City,item.State,item.Pincode);
         }
     }
 }
